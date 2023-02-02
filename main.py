0d8e3ef29aea7c9284b6350acfbdd64ef4529406
LunaBruno <110732294+LunaBruno@users.noreply.github.com>
2023-02-02 15:55:03 -0300

    Update main.py
    base code

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
 # AMB1430 - Programação para Engenharia Elétrica
 
 # Importar módulos e funções
+# https://github.com/TylerYep/stanfordkarel
 from stanfordkarel import *
 
 
 # Função principal
 def main():
+    move()
 
-
+    
 # Inicialização do programa
 if __name__ == "__main__":
     run_karel_program()
-
-
-# Girar para a direita
-def turn_right():
-    turn_left()
-    turn_left()
-    turn_left()
